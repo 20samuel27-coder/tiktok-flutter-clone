2c2afed9574e2639f6ee487904251f75141d0b6d
20samuel27-coder <20samuel27@gmail.com>
2026-07-24 06:23:10 +0000

    Update comment_controller.dart

--- lib/controllers/comment_controller.dart
+++ lib/controllers/comment_controller.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
-import 'package:tiktok_tutorial/constants.dart';
-import 'package:tiktok_tutorial/models/comment.dart';
+import 'package:affchat/constants.dart';
+import 'package:affchat/models/comment.dart';
 
 class CommentController extends GetxController {
   final Rx<List<Comment>> _comments = Rx<List<Comment>>([]);
